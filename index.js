d8a3fbb320d5104f642eca04dc32a7adc3f68050
tangytang <ivan@aceplp.com.sg>
2023-04-03 18:06:18 +0800

    Add to public component API

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ export { default as Button } from "./src/components/button";
 export { default as Heading } from "./src/components/heading";
 export { default as Section } from "./src/components/section";
 export { default as Container } from "./src/components/container";
+export { default as DevDebug } from "./src/components/dev-debug";
+export { default as Layout } from "./src/components/layout";
+export { default as Page } from "./src/components/page";
+
